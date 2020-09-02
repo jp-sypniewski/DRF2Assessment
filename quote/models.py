142596046e05923f70eb37313797481e4c72d006
jp-sypniewski <jpsypniewski@icloud.com>
2020-09-02 08:29:21 -0600

    create quote model, override __str__

--- quote/models.py
+++ quote/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Quote(models.Model):
-    
+    quote_author = models.CharField(max_length=120)
+    quote_body = models.TextField()
+    context = models.CharField(max_length=120, null=True)
+    source = models.CharField(max_length=120, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.quote_author} {self.id}"
