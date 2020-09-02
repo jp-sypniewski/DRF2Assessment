5cbd83e0d9efefe40bb37a5ff64efe6020744139
jp-sypniewski <jpsypniewski@icloud.com>
2020-09-02 08:58:33 -0600

    create custom pagination extending PageNumberPagination, add to ListCreate view

---
+++ quote/api/pagination.py
@@ -0,0 +1,4 @@
+from rest_framework.pagination import PageNumberPagination
+
+class QuotePagination(PageNumberPagination):
+    page_size = 30
--- quote/api/views.py
+++ quote/api/views.py
@@ -3,12 +3,14 @@ from rest_framework import generics
 from quote.models import Quote
 from quote.api.serializers import QuoteSerializer
 from quote.api.permissions import IsAdminUserOrReadOnly
+from quote.api.pagination import QuotePagination
 
 
 class QuoteListCreateAPIView(generics.ListCreateAPIView):
     queryset = Quote.objects.all()
     serializer_class = QuoteSerializer
     permission_classes = [IsAdminUserOrReadOnly]
+    pagination_class = QuotePagination
 
 
 class QuoteDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
